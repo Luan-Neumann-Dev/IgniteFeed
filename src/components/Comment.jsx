8e850e416e49aef67817b777668f4aae7ddaccc6
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-04-30 17:00:42 -0300

    feat: Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css'
+
+import {Trash, ThumbsUp} from '@phosphor-icons/react'
+
+export function Comment(){
+    return (
+        <div className={styles.comment}>
+            <img className={styles.avatar} src="https://github.com/Luan-Neumann-Dev.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Luan Henrique Neumann</strong>
+                            <time title="30 de Abril às 15:15h" dateTime="2025-04-30 15:15:00">Cerca de 1h atrás</time>
+                        </div>
+
+                        <button>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+
+                    <p>Muito bom Devon, parabéns!! 👏👏</p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        Aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
