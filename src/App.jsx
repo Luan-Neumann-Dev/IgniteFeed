c4ea8f6d2f468ee25704c65725e981a7ef4064a6
Luan Henrique Neumann <luan.neumann.dev@gmail.com>
2025-04-30 15:21:28 -0300

    feat: Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { Header } from "./components/Header";
+
 import { Post } from "./Post";
 
 import "./global.css"
@@ -5,6 +7,8 @@ import "./global.css"
 export function App() {
   return (
     <div>
+      <Header />
+
       <Post
         author="Diego Fernandes"
         content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
